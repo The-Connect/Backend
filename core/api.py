81dd12500349ccca88eb63f1ffb5908b82d44ec2
LovelyG7 <nia.gayle@gmail.com>
2023-10-22 12:19:54 -0400

    user profile model and post for registration 1st draft

--- core/api.py
+++ core/api.py
@@ -1,7 +1,32 @@
 from ninja import Router
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from pydantic import BaseModel
+from datetime import datetime
 
 router = Router()
 
 @router.get("/home")
 def home(request):
     return "ninja ninja"
+
+class RegistrationModel(BaseModel):
+    username: str
+    email: str
+    age: int
+    password: str
+
+#create new user profile
+@router.post("/register")
+def register_user(request, registration_data: RegistrationModel):
+    user = User.objects.create_user(
+        username=registration_data.username,
+        email=registration_data.email,
+        password=registration_data.password
+    )
+    user.profile.age = registration_data.age
+    user.profile.registration_timestamp = datetime.now()
+    user.profile.save()
+    user.save()
+
+    return{"message": "You did it!"}
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# for every user in the User table, there can be only one corresponding profile in the Profile table. If a user is deleted, the CASCADE option ensures that the associated profile is also deleted.
+#Null is allowed for age- if people don't answer it, they can still register
+#timestamp gets created at registration
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField(null=True, blank=True)
+    registration_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 
 # Create your models here.
