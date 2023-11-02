46a4294fa11de104c7d56319508dfdb6735e8852
snelzing <39928924+snelzing@users.noreply.github.com>
2023-11-02 18:17:40 -0400

    Added changes mike suggested about the models

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,26 @@
 # admin.py in your app
 from django.contrib import admin
-from .models import Question, UserResponse
+from .models import Question, FixedAnswer, UserResponse
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('answer1', 'answer2', 'answer3', 'answer4', 'answer4', 'created_at')
+    list_display = ('Title_of_Question', 'answer_1', 'created_at')
     # You can customize the admin options for the Question model here.
 
-class AnswerInline(admin.TabularInline):
-    model = UserResponse.selected_answers.through
-    extra = 1
+
+class AnswersAdmin(admin.ModelAdmin):
+    list_display = ('answer_number', 'answer_choice', 'question_id')
+
+
+# class AnswerInline(admin.TabularInline):
+#     model = UserResponse.selected_answers.through
+#     extra = 1
 
 class UserResponseAdmin(admin.ModelAdmin):
-    list_display = ('user', 'question', 'timestamp')
-    inlines = [AnswerInline]
+    list_display = ('user', 'answer', 'timestamp')
+    # inlines = [AnswerInline]
+
 
 admin.site.register(UserResponse, UserResponseAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(FixedAnswer, AnswersAdmin)
--- core/models.py
+++ core/models.py
@@ -2,20 +2,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Question(models.Model):
-    answer1 = models.TextField()
-    answer2 = models.TextField()
-    answer3 = models.TextField()
-    answer4 = models.TextField()
-    answer5 = models.TextField()
-    
-    created_at = models.DateTimeField(auto_now_add=True)
+
+# look here for inspiration - https://github.com/tomwalker/django_quiz/blob/master/quiz/models.py
+
+class FixedAnswer(models.Model):
+    answer_number = models.SmallIntegerField(blank=True, null=True)
+    answer_choice = models.CharField(max_length=25, blank=True, null=True)
+    question_id = models.ForeignKey('Question', on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'answer_choice'
 
     def __str__(self):
-        return self.text
+        return self.answer_choice
+
+
+class Question(models.Model):
+    Title_of_Question = models.TextField()
+    answer_1 = models.ForeignKey(FixedAnswer, on_delete=models.CASCADE, related_name='answer', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'question'
+
+    def __str__(self):
+        # This needs to match one of the fields so django knows what to name it.
+        return self.Title_of_Question
+
 
 class UserResponse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(FixedAnswer, on_delete=models.CASCADE, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        managed = True
+        db_table = 'user_response'
